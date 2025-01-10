022ac2aef187c78242da6d8b5f50f38046bb6664
crisis-argha <namespace.arg@gmail.com>
2025-01-10 22:44:41 +0530

    player left right movement

--- lib/actors/player.dart
+++ lib/actors/player.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 
 import 'package:flame/components.dart';
+import 'package:flutter/src/services/hardware_keyboard.dart';
+import 'package:flutter/src/services/keyboard_key.g.dart';
 import 'package:pixel_adventure/pixel_adventure.dart';
 
 enum PlayerState {
@@ -16,7 +18,7 @@ enum PlayerDirection {
 }
 
 class Player extends SpriteAnimationGroupComponent
-    with HasGameRef<PixelAdventure> {
+    with HasGameRef<PixelAdventure>, KeyboardHandler {
   String character;
 
   Player({position, required this.character}) : super(position: position);
@@ -28,8 +30,9 @@ class Player extends SpriteAnimationGroupComponent
   final double stepTime = 0.05;
 
   PlayerDirection playerDirection = PlayerDirection.none;
-  double moveSpeed = 1;
+  double moveSpeed = 100;
   Vector2 velocity = Vector2.zero();
+  bool isFacingRight = true;
 
   @override
   FutureOr<void> onLoad() {
@@ -41,6 +44,24 @@ class Player extends SpriteAnimationGroupComponent
   void update(double dt) {
     _updatePlayerMovement(dt);
     super.update(dt);
+  }
+
+  @override
+  bool onKeyEvent(KeyEvent event, Set<LogicalKeyboardKey> keysPressed) {
+    final isLeftKeyPressed = keysPressed.contains(LogicalKeyboardKey.keyA);
+    final isRightKeyPressed = keysPressed.contains(LogicalKeyboardKey.keyDa);
+
+    if (isRightKeyPressed && isLeftKeyPressed) {
+      playerDirection = PlayerDirection.none;
+    } else if (isLeftKeyPressed) {
+      playerDirection = PlayerDirection.left;
+    } else if (isRightKeyPressed) {
+      playerDirection = PlayerDirection.right;
+    } else {
+      playerDirection = PlayerDirection.none;
+    }
+
+    return super.onKeyEvent(event, keysPressed);
   }
 
   void _loadAllAnimations() {
@@ -72,16 +93,27 @@ class Player extends SpriteAnimationGroupComponent
     double dirX = 0.0;
     switch (playerDirection) {
       case PlayerDirection.left:
+        if (isFacingRight) {
+          flipHorizontallyAroundCenter();
+          isFacingRight = false;
+        }
+        current = PlayerState.running;
         dirX -= moveSpeed;
         break;
       case PlayerDirection.right:
+        if (!isFacingRight) {
+          flipHorizontallyAroundCenter();
+          isFacingRight = true;
+        }
+        current = PlayerState.running;
         dirX += moveSpeed;
         break;
       case PlayerDirection.none:
+        current = PlayerState.idle;
         break;
       default:
     }
     velocity = Vector2(dirX, 0.0);
-    position += velocity;
+    position += velocity * dt;
   }
 }
--- lib/pixel_adventure.dart
+++ lib/pixel_adventure.dart
@@ -1,11 +1,12 @@
 import 'dart:async';
 
 import 'package:flame/components.dart';
+import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:pixel_adventure/levels/levels.dart';
 
-class PixelAdventure extends FlameGame {
+class PixelAdventure extends FlameGame with HasKeyboardHandlerComponents {
   @override
   Color backgroundColor() => const Color(0xff211f30);
 
