82aeede81ae3e866c77882622083b1db12259f5b
crisis-argha <namespace.arg@gmail.com>
2025-01-31 12:07:50 +0530

    hit box added in fire trap

--- lib/components/traps/fire.dart
+++ lib/components/traps/fire.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 
+import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
+import 'package:pixel_adventure/components/player_hitbox.dart';
 import 'package:pixel_adventure/pixel_adventure.dart';
 
 class Fire extends SpriteAnimationComponent with HasGameRef<PixelAdventure> {
@@ -12,8 +14,24 @@ class Fire extends SpriteAnimationComponent with HasGameRef<PixelAdventure> {
 
   final stepTime = 0.05;
 
+  final hitbox = CustomHitbox(
+    offsetX: 0,
+    offsetY: 16,
+    width: 16,
+    height: 16,
+  );
+
   @override
   FutureOr<void> onLoad() {
+    debugMode = true;
+      add(
+      RectangleHitbox(
+        position: Vector2(hitbox.offsetX, hitbox.offsetY),
+        size: Vector2(hitbox.width, hitbox.height),
+        collisionType: CollisionType.active,
+      ),
+    );
+
     animation = SpriteAnimation.fromFrameData(
       game.images.fromCache('Traps/Fire/Off.png'),
       SpriteAnimationData.sequenced(
