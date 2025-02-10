d8ce397f0e340b7f52333b30052a815a62a7f71a
crisis-argha <namespace.arg@gmail.com>
2025-02-10 23:44:22 +0530

    update rock head state

--- lib/components/traps/rock_head.dart
+++ lib/components/traps/rock_head.dart
@@ -3,7 +3,15 @@ import 'dart:async';
 import 'package:flame/components.dart';
 import 'package:pixel_adventure/pixel_adventure.dart';
 
-class RockHead extends SpriteAnimationComponent
+enum RockHeadState {
+  idle,
+  bottomHit,
+  topHit,
+  leftHit,
+  rightHit,
+}
+
+class RockHead extends SpriteAnimationGroupComponent
     with HasGameRef<PixelAdventure> {
   RockHead({
     position,
@@ -14,18 +22,41 @@ class RockHead extends SpriteAnimationComponent
         );
 
   double stepTime = 0.05;
+  late final SpriteAnimation idleAnimation;
+
+  @override
+  void update(double dt) {
+    _rockHeadState();
+    super.update(dt);
+  }
 
   @override
   FutureOr<void> onLoad() {
     debugMode = true;
-    animation = SpriteAnimation.fromFrameData(
-      game.images.fromCache('Traps/Rock Head/Blink (42x42).png'),
+    _loadAllAnimations();
+    return super.onLoad();
+  }
+
+  void _loadAllAnimations() {
+    idleAnimation = _spriteAnimation('Idle', 1);
+    animations = {
+      RockHeadState.idle: idleAnimation,
+    };
+  }
+
+  SpriteAnimation _spriteAnimation(String state, int amount) {
+    return SpriteAnimation.fromFrameData(
+      game.images.fromCache('Traps/Rock Head/$state.png'),
       SpriteAnimationData.sequenced(
-        amount: 4,
+        amount: amount,
         stepTime: stepTime,
         textureSize: Vector2.all(42),
       ),
     );
-    return super.onLoad();
+  }
+
+  void _rockHeadState() {
+    RockHeadState rockHeadState = RockHeadState.idle;
+    current = rockHeadState;
   }
 }
